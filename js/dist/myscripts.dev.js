4f2c2ead0e4cadd341f90a9798afb4db1f8713e7
nbon <nadabonaso@gmail.com>
2024-02-29 15:34:46 +0200

    moved hp js to index

--- js/dist/myscripts.dev.js
+++ js/dist/myscripts.dev.js
@@ -14,45 +14,4 @@ document.addEventListener('DOMContentLoaded', function () {
       $target.classList.toggle('is-active');
     });
   });
-}); // Slider tesimonials
-
-var index = 0;
-document.getElementById("restart").setAttribute('disabled', '');
-show_testimonial(index);
-
-function show_testimonial(i) {
-  index += i;
-  var testimonials = document.getElementsByClassName("testimonial");
-  var dots = document.getElementsByClassName("dot");
-
-  for (i = 0; i < testimonials.length; i++) {
-    testimonials[i].style.display = "none";
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  if (index > testimonials.length - 1) index = 0;
-  if (index < 0) index = testimonials.length - 1;
-  testimonials[index].style.display = "block";
-  dots[index].className += " active";
-}
-
-var myInterval = setInterval(startAni, 10000);
-
-function startAni() {
-  document.getElementById("restart").setAttribute('disabled', '');
-  document.getElementById("stop").removeAttribute('disabled');
-  document.getElementById("next").click();
-}
-
-function stopAni() {
-  document.getElementById("stop").setAttribute('disabled', '');
-  document.getElementById("restart").removeAttribute('disabled');
-  clearInterval(myInterval);
-}
-
-function reStartAni() {
-  location.reload();
-}
+});
