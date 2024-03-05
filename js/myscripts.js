b126cdb7f00f101f617c4e9be39f3bdcf19b0c8f
nbon <nadabonaso@gmail.com>
2024-03-05 08:56:24 +0200

    Cleaned up code with Prettier and new Live Server testing

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,70 +1,67 @@
 // Navbar menu
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-
-            // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-
-        });
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
     });
-
+  });
 });
 
 // Slider tesimonials
 let index = 0;
-document.getElementById("restart").setAttribute('disabled', '');
+document.getElementById("restart").setAttribute("disabled", "");
 show_testimonial(index);
 
 function show_testimonial(i) {
-    index += i;
+  index += i;
 
-    let testimonials = document.getElementsByClassName("testimonial");
+  let testimonials = document.getElementsByClassName("testimonial");
 
-    let dots = document.getElementsByClassName("dot");
+  let dots = document.getElementsByClassName("dot");
 
-    for (i = 0; i < testimonials.length; i++) {
-        testimonials[i].style.display = "none";
-    }
+  for (i = 0; i < testimonials.length; i++) {
+    testimonials[i].style.display = "none";
+  }
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
 
-    if (index > testimonials.length - 1)
-        index = 0;
+  if (index > testimonials.length - 1) index = 0;
 
-    if (index < 0)
-        index = testimonials.length - 1;
+  if (index < 0) index = testimonials.length - 1;
 
-    testimonials[index].style.display = "block";
-    dots[index].className += " active";
+  testimonials[index].style.display = "block";
+  dots[index].className += " active";
 }
 
 let myInterval = setInterval(startAni, 10000);
 
 function startAni() {
-    document.getElementById("restart").setAttribute('disabled', '');
-    document.getElementById("stop").removeAttribute('disabled');
-    document.getElementById("next").click();
+  document.getElementById("restart").setAttribute("disabled", "");
+  document.getElementById("stop").removeAttribute("disabled");
+  document.getElementById("next").click();
 }
 
 function stopAni() {
-    document.getElementById("stop").setAttribute('disabled', '');
-    document.getElementById("restart").removeAttribute('disabled');
-    clearInterval(myInterval);
+  document.getElementById("stop").setAttribute("disabled", "");
+  document.getElementById("restart").removeAttribute("disabled");
+  clearInterval(myInterval);
 }
 
 function reStartAni() {
-    location.reload();
+  location.reload();
 }
